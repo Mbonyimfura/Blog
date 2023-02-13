fa3d056aa00674ec48851bd23e5775fdd3197539
mbonyimfura <mbonyimfuratresor@gmail.com>
2023-02-14 01:08:14 +0200

    adding a way to send emil

---
+++ src/emails/account.js
@@ -0,0 +1,15 @@
+const sgMail=require('@sendgrid/mail')
+require('dotenv').config()
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const sendWelcomeEmail=(email,username)=>{
+    sgMail.send({
+        to:"mbonyimfuratresor@gmail.com",
+       from:"mbonyimfuratresor@gmail.com",
+       subject:'Thank you for joining in!',
+       text:`Welcome to the app, ${username}. Let me know how you get along with the app `
+    })
+}
+module.exports={
+    sendWelcomeEmail
+}
